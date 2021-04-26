1e49c063a48a1581072700a709defef6230469df
Christopher Hui <christophergkhui@gmail.com>
2021-04-26 00:19:33 -0700

    experience

--- src/components/home.component.tsx
+++ src/components/home.component.tsx
@@ -4,16 +4,17 @@ import * as React from 'react';
 import { Image } from 'react-bootstrap';
 import { BLUE, RED } from './colors';
 import doge from '../assets/doge.png';
+import { ReactComponent as Coursera } from '../assets/coursera.svg';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 const rcHomeContainer = css({
   marginTop: '3em',
-  marginRight: '10%',
-  marginLeft: '10%'
 });
 
 const aboutMe = css({
+  marginRight: '10%',
+  marginLeft: '10%',
   background: BLUE,
   borderRadius: '0.75em',
   padding: '1em 2em',
@@ -41,9 +42,15 @@ const intro = css({
   width: '80%'
 });
 
+const expBackground = css({
+  backgroundColor: 'grey'
+});
+
 const experience = css({
   marginTop: '2em',
   marginBottom: '2em',
+  marginRight: '10%',
+  marginLeft: '10%',
 });
 
 const expTitle = css({
@@ -68,21 +75,25 @@ const Home = () => {
           </ul>
         </div>
       </div>
-      <div css={experience}>
-        <h1 css={expTitle}>Experience</h1>
-        <VerticalTimeline>
-          <VerticalTimelineElement
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-            date="2011 - present"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+      <div css={expBackground}>
+        <div css={experience}>
+          <h1 css={expTitle}>Experience</h1>
+          <VerticalTimeline>
+            <VerticalTimelineElement
+              contentStyle={{ background: '#0056d2', color: '#FFF' }}
+              contentArrowStyle={{ borderRight: '7px solid  #0056d2' }}
+              date="Jan 2021 - Apr 2021"
+              icon={<Coursera style={{ height: 40, width: 40, marginTop: '-20px', marginLeft: '-20px' }} />}
+              iconStyle={{ background: '#0056d2', color: '0056d2' }}>
+              <h3>Coursera</h3>
+              <h4>Software Engineer Intern</h4>
+              <h4>Toronto, ON</h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
             </p>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
       </div>
     </div>
   );
